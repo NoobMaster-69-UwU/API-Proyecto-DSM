1a7e1e495dd05e08750c2c53da96ff3672e78d89
Carlos Campos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2025-11-19 00:16:24 -0600

    Delete api_vercel directory

--- api_vercel/api/attend/attendees.js
+++
@@ -1,9 +0,0 @@
-import { db } from "../../firebase";
-
-export default async function handler(req,res){
-  if(req.method!=="GET")return res.status(405).json({message:"Método no permitido"});
-
-  const {eventId}=req.query;
-  const snap=await db.collection("events").doc(eventId).collection("attendees").get();
-  return res.json(snap.docs.map(d=>({id:d.id,...d.data()})));
-}
